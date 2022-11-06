7b922529f484411fb129605248daaa3064bb4bf8
Starboyzs <ojoayowale3@gmail.com>
2022-11-06 09:11:18 +0100

    updated package.json

--- src/Components/Portfolio/Portfolio.jsx
+++ src/Components/Portfolio/Portfolio.jsx
@@ -1,71 +1,64 @@
-import React from 'react'
-import"./Portfolio.css"
-import CTA from "../Header/CTA"
-import IMG1 from "../../assets/portfolio1.jpg"
-import IMG2 from "../../assets/portfolio2.jpg"
-import IMG3 from "../../assets/portfolio3.jpg"
-import IMG4 from "../../assets/portfolio4.jpg"
-import IMG5 from "../../assets/portfolio5.png"
-import IMG6 from "../../assets/portfolio6.jpg"
+import React from "react";
+import "./Portfolio.css";
+import CTA from "../Header/CTA";
+import IMG1 from "../../assets/portfolio1.jpg";
+import IMG2 from "../../assets/portfolio2.jpg";
+import IMG3 from "../../assets/portfolio3.jpg";
+import IMG4 from "../../assets/portfolio4.jpg";
+import IMG5 from "../../assets/portfolio5.png";
+import IMG6 from "../../assets/portfolio6.jpg";
 
-const data=[
+const data = [
   {
-    id:1,
-    image:IMG1,
-    text:"Google Keeper App Clone",
-    alt:"Google Keeper App Clone image",
-    link1:"https://github.com/Starboyzs/keeper-app-clone.git",
-    link2:"https://starboyzs.github.io/keeper-app-clone/"
-
+    id: 1,
+    image: IMG1,
+    text: "Google Keeper App Clone",
+    alt: "Google Keeper App Clone image",
+    link1: "https://github.com/Starboyzs/keeper-app-clone.git",
+    link2: "https://taupe-frangipane-b94292.netlify.app/",
   },
   {
-    id:2,
-    image:IMG2,
-    text:"React Portfolio Website",
-    alt:"React Portfolio website image",
-    link1:"https://github.com/Starboyzs/portfolio.git",
-    link2:"https://starboyzs.github.io/portfolio/"
-
+    id: 2,
+    image: IMG2,
+    text: "React Portfolio Website",
+    alt: "React Portfolio website image",
+    link1: "https://github.com/Starboyzs/portfolio.git",
+    link2: "https://clever-cactus-5e75a8.netlify.app/",
   },
   {
-    id:3,
-    image:IMG3,
-    text:"Bootstrap Portfolio Website",
-    alt:"React Portfolio website image",
-    link1:"https://github.com/Starboyzs/Starboyzs.github.io",
-    link2:"https://github.com/Starboyzs/Starboyzs.github.io"
-
+    id: 3,
+    image: IMG3,
+    text: "Api Fetcher",
+    alt: "React Portfolio website image",
+    link1: "https://github.com/Starboyzs/TalentQl.git",
+    link2: "https://lustrous-muffin-573780.netlify.app/",
   },
 
   {
-    id:4,
-    image:IMG4,
-    text:"TinDog App Clone",
-    alt:"TinDog app clone image",
-    link1:"https://github.com/Starboyzs/tindog.git",
-    link2:"https://starboyzs.github.io/tindog/"
-
+    id: 4,
+    image: IMG4,
+    text: "Omni Food Website",
+    alt: "OMNI FOOD image",
+    link1: "https://github.com/Starboyzs/foodPalace.git",
+    link2: "https://heartfelt-sorbet-a59dc6.netlify.app/",
   },
   {
-    id:5,
-    image:IMG5,
-    text:"My Site",
-    alt:"My-site image",
-    link1:"https://github.com/Starboyzs/my-site.git",
-    link2:"https://starboyzs.github.io/my-site/"
-
+    id: 5,
+    image: IMG5,
+    text: "My Site",
+    alt: "My-site image",
+    link1: "https://github.com/Starboyzs/my-site.git",
+    link2: "https://harmonious-faloodeh-c9d49e.netlify.app/",
   },
   {
-    id:6,
-    image:IMG6,
-    text:"CV",
-    alt:"CV image",
-    link1:"https://github.com/Starboyzs/CV.git",
-    link2:"https://starboyzs.github.io/CV/"
-
-  }
-  
-]
+    id: 6,
+    image: IMG6,
+    text: "Phone Number Combination",
+    alt: "Number combination image",
+    link1: "https://github.com/Starboyzs/numberCombination.git",
+    link2: "https://startling-ganache-b2c2c6.netlify.app/",
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -73,32 +66,30 @@ const Portfolio = () => {
       <h5>My recent work</h5>
       <h2>Portfolio</h2>
 
-      <div className='container portfolio__container'>
-      {data.map((item)=>{
-        return(
-          <article className='portfolio__item'>
-        <div className='portfolio__item-image'> 
-        <img src= {item.image} alt={item.alt}/>
-        </div>
-        <h3>{item.text}</h3>
+      <div className="container portfolio__container">
+        {data.map((item) => {
+          return (
+            <article className="portfolio__item">
+              <div className="portfolio__item-image">
+                <img src={item.image} alt={item.alt} />
+              </div>
+              <h3>{item.text}</h3>
 
-        <div className='portfolio__item-cta'>
-        <CTA
-          text1={"Repository"}
-          text2={"live Demo"}
-          link1={item.link1}
-          link2={item.link2}
-          target={"_blank"}
-        />
-        </div>
-        
-        </article>)
-      })}
-       
+              <div className="portfolio__item-cta">
+                <CTA
+                  text1={"Repository"}
+                  text2={"live Demo"}
+                  link1={item.link1}
+                  link2={item.link2}
+                  target={"_blank"}
+                />
+              </div>
+            </article>
+          );
+        })}
 
         {/* <a href="https://github.com/Starboyzs/Starboyzs.github.io" className="btn ">Repository</a>
         <a href="https://github.com/Starboyzs/Starboyzs.github.io" className="btn btn-primary">Live Demo</a> */}
-        
 
         {/* <article className='portfolio__item'>
         <div className='portfolio__item-image'> 
@@ -154,10 +145,9 @@ const Portfolio = () => {
         <a href="https://github.com/Starboyzs/Starboyzs.github.io" className="btn btn-primary">Live Demo</a>
         </div>
         </article> */}
-       
       </div>
     </section>
-  )
-}
+  );
+};
 
-export default Portfolio
+export default Portfolio;
